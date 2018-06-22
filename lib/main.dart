805ab8aedd4b282357abc69113065d4aef1a6a83
Burak Tamtürk <buraktamturk@gmail.com>
2018-06-23 00:18:59 +0300

    stuff

--- lib/main.dart
+++ lib/main.dart
@@ -3,40 +3,20 @@ import 'package:flutter/material.dart';
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
       title: 'Flutter Demo',
       theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
+      home: new MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  MyHomePage({Key key}) : super(key: key);
 
   @override
   _MyHomePageState createState() => new _MyHomePageState();
@@ -68,7 +48,7 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: new AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: new Text(widget.title),
+        title: new Text("U-Wallet"),
       ),
       body: new Center(
         // Center is a layout widget. It takes a single child and positions it
@@ -87,23 +67,64 @@ class _MyHomePageState extends State<MyHomePage> {
           // center the children vertically; the main axis here is the vertical
           // axis because Columns are vertical (the cross axis would be
           // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-            new Text(
-              'You have pushed the button this many times:',
+
+
+            new Padding(
+              padding: const EdgeInsets.only(top: 24.0),
+              child: new Column(
+                children: [
+                  new Text(
+                      'You have'
+                  ),
+
+                  new Text(
+                    '0€',
+                    style: Theme.of(context).textTheme.display3,
+                  ),
+
+                  new Text(
+                      'on your offline account.'
+                  ),
+
+                ]
+              ),
             ),
-            new Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+
+
+            new MaterialButton(
+              onPressed: () {
+
+              },
+
+              child: new Text("SEND MONEY"),
+            ),
+
+            new FlatButton(
+              onPressed: () {
+
+              },
+              child: new Text("RECEIVE MONEY"),
+            ),
+
+            new FlatButton(
+              onPressed: () {
+
+              },
+              child: new Text("LOAD IT FROM BANK ACCOUNT"),
+            ),
+
+            new FlatButton(
+              onPressed: () {
+
+              },
+              child: new Text("TRANFER TO BANK ACCOUNT"),
             ),
           ],
         ),
       ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: new Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+
     );
   }
 }
