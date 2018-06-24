20aa67f006be467459e3c8f6f3cf64c085bea1ce
Burak Tamtürk <buraktamturk@gmail.com>
2018-06-24 14:41:19 +0300

    removal of transfer page

--- lib/account.dart
+++ lib/account.dart
@@ -2,7 +2,6 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'tranfer.dart';
 import 'package:qrcode_reader/QRCodeReader.dart';
 import 'dart:async';
 import 'database.dart';
